4555e0b5c80be6bf87ea6133bb9c1fabf210ad59
Yewhral <zabrzyjewski@gmail.com>
2017-07-12 10:56:52 +0200

    Final release

---
+++ build/js/warnings.js
@@ -0,0 +1,33 @@
+'use strict';
+
+function createBubble(text) {
+    // creates a popup bubble that shows up after failed validaiton
+    var bubble = document.createElement('div');
+    var parent = document.getElementById("bubble_holder");
+    bubble.innerHTML = text;
+    bubble.setAttribute('class', 'message_bubble');
+    if (text.length <= 45) {
+        bubble.style.cssText = "padding-top:10px; min-height:30px;";
+    }
+    parent.insertBefore(bubble, parent.childNodes[0]);
+
+    setTimeout(function () {
+        bubble.style.opacity = 0;
+    }, 500);
+
+    setTimeout(function () {
+        parent.removeChild(parent.lastChild);
+    }, 4500);
+}
+
+function createWarning(field, warning, limit) {
+    // pops up a message about excessive amount of accepted signs in a field
+    var box = document.getElementsByName(field)[0].value;
+    var textWarning1 = document.getElementById(warning);
+    if (box.length > limit) {
+        textWarning1.style.display = 'inline';
+        textWarning1.innerHTML = 'This field cannot contain more than ' + limit + ' signs. Currently you have ' + (box.length - limit) + ' too many.';
+    } else {
+        textWarning1.style.display = 'none';
+    }
+}
